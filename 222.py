7ae86df7b6888c0ceb420540f009aeed90e29f52
DESKTOP-E6BUTSB\Анатолий <tolya.ponomarenko.89@ukr.net>
2024-01-03 23:23:57 +0300

    initial commit

---
+++ 222.py
@@ -0,0 +1,15 @@
+# num1 = float(input("Enter first number"))
+# num2 = float(input("Enter second number"))
+# num3 = float(input("enter third number"))
+# operation = input("choose an operation (max,min,avg);")
+# if operation == "max":
+#     result = max("nam1,nam2,nam3")
+#     print(f"max:({result}")
+# elif operation == "min":
+#     result =min("nam1,nam2,nam3")
+#     print(f"min:({result}")
+# elif operation == "avg":
+#     result =("nam1+nam2+nam3")/3
+#     print(f"arithmetic mean:{result}")
+# else:
+#     print("invalid operation.please choose 'max' , 'min' or 'avg'.")
